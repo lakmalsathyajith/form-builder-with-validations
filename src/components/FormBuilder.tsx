854999ef7e196db4dd3887dce3d2a8fb176a87ca
Sathyajith <slakmal68@gmail.com>
2023-08-21 11:33:44 +0530

    fix ts errors

--- src/components/FormBuilder.tsx
+++ src/components/FormBuilder.tsx
@@ -43,7 +43,8 @@ export const FormBuilder = () => {
   };
 
   const disabled =
-    fields[currentKey] !== undefined || currentKey === ''
+    (fields[currentKey] !== undefined && currentType === '') ||
+    currentKey === ''
       ? 'disabled'
       : undefined;
 
